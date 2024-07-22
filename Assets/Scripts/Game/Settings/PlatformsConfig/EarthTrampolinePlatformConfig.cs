5561a4939ba710a7d76c17d4d0de1e9cfd73e345
abromus <abromus@mail.ru>
2024-07-22 21:14:43 +0300

    Мир: платформа с батутом + анимация

---
+++ Assets/Scripts/Game/Settings/PlatformsConfig/EarthTrampolinePlatformConfig.cs
@@ -0,0 +1,34 @@
+﻿using DoodleJump.Game.Worlds;
+using UnityEngine;
+
+namespace DoodleJump.Game.Settings
+{
+    [System.Serializable]
+    internal struct EarthTrampolinePlatformConfig : IPlatformConfig, ISpringJumpConfig
+    {
+        [SerializeField] private Platform _platformPrefab;
+        [SerializeField] private float _spawnChance;
+        [SerializeField] private TriggerType _triggerType;
+        [SerializeField] private int _defaultJumpForce;
+        [SerializeField] private int _springJumpForce;
+
+        public readonly string Title => "Конфиг земляной платформы с батутом";
+
+        public readonly Platform PlatformPrefab => _platformPrefab;
+
+        public readonly float SpawnChance => _spawnChance;
+
+        public readonly TriggerType TriggerType => _triggerType;
+
+        public readonly float JumpForce => _defaultJumpForce;
+
+        public readonly float SpringJumpForce => _springJumpForce;
+
+#if UNITY_EDITOR
+        public void ChangeSpawnChance(float factor)
+        {
+            _spawnChance *= factor;
+        }
+#endif
+    }
+}
